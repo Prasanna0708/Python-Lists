09c9c69680ef3192f65af7f27b8a6ba700a758e5
Prasanna0708 <78546913+Prasanna0708@users.noreply.github.com>
2021-07-30 14:33:50 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,26 @@
+#In python List are created by [],and list are ordered,changable,duplicates allows 
+'''num = [5,6,7,9,8,1,1,4,3,2,8,9,10]
+num.sort()
+print(num)
+print(len(num))
+print(num[2:5])
+print(num[-1])'''
+
+
+x = ["apple","banana",1,1,1.0,"orange"]
+print(x)
+y = [1,2,3,4,5,6]
+print(y.sort(reverse = True))
+print(y)
+print(max(y))
+#append is add elements in the end of the list
+print(y.append("Python"))
+print(y)
+print(y.insert(0,"orange"))
+print(y)
+print(y.pop(1))
+print(y)
+print(y.extend(x))
+print(y)
+print(x.count(1))
+print(x)
